49c061f8420364f1f7325cb1a25697f00c14683a
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2015-10-06 19:24:43 +0300

    chore(test): small update

--- test/quasar-cli.js
+++ test/quasar-cli.js
@@ -4,42 +4,41 @@ var exec = require('child_process').exec;
 var path = require('path');
 var expect = require('chai').expect;
 
-describe('quasar-cli bin', function(){
-	var cmd = 'node '+path.join(__dirname, '../bin/quasar-cli')+' ';
-	console.log(cmd);
+describe('main functionality', function(){
+    var cmd = 'node '+path.join(__dirname, '../bin/quasar-cli')+' ';
+    console.log(cmd);
 
-	it('--help should run without errors', function(done) {
-		exec(cmd+'--help', function (error, stdout, stderr) {
-			expect(error).to.not.exist;
-			done();
-		});
-	});
+    it('--help should run without errors', function(done) {
+        exec(cmd+'--help', function (error, stdout, stderr) {
+            expect(error).to.not.exist;
+            done();
+        });
+    });
 
-	it('--version should run without errors', function(done) {
-		exec(cmd+'--version', function (error, stdout, stderr) {
-			expect(error).to.not.exist;
-			done();
-		});
-	});
+    it('--version should run without errors', function(done) {
+        exec(cmd+'--version', function (error, stdout, stderr) {
+            expect(error).to.not.exist;
+            done();
+        });
+    });
 
-	it('should return error on missing command', function(done) {
+    it('should return error on missing command', function(done) {
         this.timeout(4000);
 
-		exec(cmd, function (error, stdout, stderr) {
-			expect(error).to.exist;
-			expect(error.code).to.equal(1);
-			done();
-		});
-	});
+        exec(cmd, function (error, stdout, stderr) {
+            expect(error).to.exist;
+            expect(error.code).to.equal(1);
+            done();
+        });
+    });
 
-	it('should return error on unknown command', function(done) {
+    it('should return error on unknown command', function(done) {
         this.timeout(4000);
 
-		exec(cmd+'junkcmd', function (error, stdout, stderr) {
-			expect(error).to.exist;
-			expect(error.code).to.equal(1);
-			done();
-		});
-	});
-
+        exec(cmd+'junkcmd', function (error, stdout, stderr) {
+            expect(error).to.exist;
+            expect(error.code).to.equal(1);
+            done();
+        });
+    });
 });
