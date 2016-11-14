94d86df969988f731f27f4255d2825fccb9f6ad7
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2016-11-14 23:21:53 +0200

    feat: Electron wrapper

--- bin/wrappers/electron/install.js
+++ bin/wrappers/electron/install.js
@@ -1,45 +1,67 @@
 var
   path = require('path'),
+  download = require('download-github-repo'),
+  ora = require('ora'),
+  uid = require('uid'),
   log = require('../../../lib/log'),
-  qfs = require('../../../lib/qfs'),
-  spawn = require('../../../lib/spawn')
+  qfs = require('../../../lib/qfs')
+
+function generate (src, dest) {
+  qfs.copy(path.join(src, 'template'), dest)
+
+  log()
+  log.success('Elecron wrapper created at', dest.yellow + '\n')
+  log()
+  log('  To get started:')
+  log()
+  log('  ★ Change directory to the wrapper')
+  log('    $ cd electron')
+  log()
+  log('  ★ Edit package.json')
+  log()
+  log('  ★ Install dependencies')
+  log('    $ npm install')
+  log()
+  log('  ★ To start Electron app in "dev" mode:')
+  log('    $ quasar dev')
+  log('    $ npm run dev')
+  log()
+  log('  ★ To build Electron app for production:')
+  log('    $ npm run build')
+  log('    # build specific platform:')
+  log('    $ npm run build linux')
+  log()
+}
 
 module.exports = function (options) {
   var
     appPath = options.appPath,
-    src = path.join(__dirname, 'files'),
-    dest = path.join(appPath, 'electron')
+    src = '/tmp/quasar-electron-template-' + uid(),
+    dest = path.join(appPath, 'electron'),
+    spinner = ora('Downloading Quasar Electron template...')
 
-  // copy electron.js to root
-  qfs.copy(src, dest)
+  spinner.start()
+  download(
+    'quasarframework/electron-wrapper',
+    src,
+    function (err) {
+      spinner.stop()
+      process.on('exit', function () {
+        qfs.remove(src)
+      })
 
-  spawn({
-    command: 'npm',
-    args: ['i'],
-    cwd: dest,
-    callback: function (exitCode) {
-      if (exitCode !== 0) {
-        log.error('Error installing electron modules.')
-        return
+      if (err) {
+        if (err === 404) {
+          log.fatal('Electron wrapper template may have moved. Please update to latest Quasar CLI.')
+          // ^^^ EARLY EXIT
+        }
+
+        log.fatal('Failed to download Quasar Electron App template: ' + err)
+        // ^^^ EARLY EXIT
       }
 
-      log()
-      log.success('Elecron wrapper created at', dest.yellow + '\n')
-      log()
-      log('  To get started:')
-      log()
-      log('  ★ Change directory to the wrapper')
-      log('    $ cd electron')
-      log()
-      log('  ★ Start the app')
-      log('    $ npm start')
-      log()
-      log('  ★ For convenience, add a script to package.json in your project root folder'.gray)
-      log('    "electron": "cd electron && npm start"')
-      log()
-      log('  ★ To start electron app using script:')
-      log('    $ npm start:electron')
-      log()
+      log.success('Downloaded Quasar Electron App template.')
+      generate(src, dest)
     }
-  })
+  )
 }
